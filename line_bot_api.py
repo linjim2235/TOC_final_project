1bb5b222844db731338a93b136cd7ca4be32b750
Jim <83525577+linjim2235@users.noreply.github.com>
2022-12-26 20:57:50 +0800

    Update line_bot_api.py

--- line_bot_api.py
+++ line_bot_api.py
@@ -10,5 +10,5 @@ TemplateSendMessage, ImageCarouselTemplate, ImageCarouselColumn, PostbackAction,
 QuickReply, QuickReplyButton, ConfirmTemplate, ButtonsTemplate, URIAction
 )
 
-line_bot_api = LineBotApi('eSVk/98ZjxvW5BCadHNdyNw/6c6oPWjSGSxYaaQJDMuY0/Isu3khwnfaNa+XeEquayAxsmAnK2rdOPtNxrF1ARgcaSXzuQ5ire4uxiustRuVN16XHGblwLMMiGtOeFW76Du4xdLEJQ/MuDqi7kYf7QdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('12cc7e59f5a0445db01500806d70abe1')
+line_bot_api = LineBotApi('LINE_CHANNEL_SECRET')
+handler = WebhookHandler('LINE_CHANNEL_ACCESS_TOKEN')
