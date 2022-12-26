233389127dfe4760c3bed70c53e77ea169b30b50
Jim <83525577+linjim2235@users.noreply.github.com>
2022-12-26 21:42:11 +0800

    Add files via upload

---
+++ fsm.py
@@ -0,0 +1,62 @@
+from transitions.extensions import GraphMachine
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(model=self, **machine_configs)
+
+    def is_going_to_state1(self, event):
+        text = event.message.text
+        return text.lower() == "go to state1"
+
+    def is_going_to_state2(self, event):
+        text = event.message.text
+        return text.lower() == "go to state2"
+
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state1")
+        self.go_back()
+
+    def on_exit_state1(self):
+        print("Leaving state1")
+
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state2")
+        self.go_back()
+
+    def on_exit_state2(self):
+        print("Leaving state2")
+
+machine = TocMachine(
+    states=["user", "state1", "state2", "state3"],
+    transitions=[
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "state1",
+            "conditions": "is_going_to_state1",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "state2",
+            "conditions": "is_going_to_state2",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "state3",
+            "conditions": "is_going_to_state3",
+        },
+        {"trigger": "go_back", "source": ["state1", "state2", "state3"], "dest": "user"},
+    ],
+    initial="user",
+    auto_transitions=False,
+    show_conditions=True,
+)
+machine.get_graph().draw("fsm.png", prog="dot", format="png")
